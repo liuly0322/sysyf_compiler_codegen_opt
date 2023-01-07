b49e430619a3be13200d9bd571641de3161b9cfd
liuly <me@liuly.moe>
2023-01-08 03:41:07 +0800

    add: more testcases

--- SysYF_Pass_Student/test/test.py
+++ SysYF_Pass_Student/test/test.py
@@ -83,10 +83,12 @@ if __name__ == "__main__":
     TEST_DIRS = [
                 './Test_H/Easy_H/',
                 './Test_H/Medium_H/',
-                './Test_H//Hard_H/',
+                './Test_H/Hard_H/',
                 './Test/Easy/',
                 './Test/Medium/',
-                './Test/Hard/'
+                './Test/Hard/',
+                './function_test2020/',
+                './function_test2021/'
                 ]
     # you can only modify this to add your testcase
 
